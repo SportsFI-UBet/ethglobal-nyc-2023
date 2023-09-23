0e65a1e3522daf129dbfc50274e2b0291c6a3309
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2023-09-23 16:45:54 -0400

    Login with just privy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    nodePolyfills({
+      include: ["util", "buffer"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
+  server: {
+    open: true,
+    // https: true
+  },
 });
